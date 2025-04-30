ef450270d9a7099d4ffaaf7e2471fef0e56491c9
Pablo Troli <brawlixo123@gmail.com>
2025-04-30 02:13:01 -0300

    🚀 (api) Integrates database with endpoint `/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,9 @@
-function status(request, resposne) {
+import database from "../../../../infra/database.js";
+
+
+async function status(request, resposne) {
+  const result = await database.query('SELECT 2 + 2 as soma;');
+  console.log(result.rows)
   resposne.status(200).send("Ficando perigoso como desenvolverdor na web")
 }
 
