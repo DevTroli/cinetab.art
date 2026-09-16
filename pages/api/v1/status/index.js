1b0cd96ada5c18226305af3a5bd8670252d89c7e
Troli <brawlixo123@gmail.com>
2026-09-16 08:01:47 -0300

    chore(infra): add database.js

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status (request, response) {
+import database from "../../../../infra/database.js"
+
+async function status (request, response) {
+  const res = await database.query("SELECT 20+20+20+7 as aura;");
+  console.log(res.rows);
   response.status(200).json({saude:"OK!"})
 }
 
